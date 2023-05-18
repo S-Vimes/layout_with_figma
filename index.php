832ffe1a6207f8e241e98e42392270d3b028360e
Ryzhov Maxim <62740559+S-Vimes@users.noreply.github.com>
2023-05-18 21:17:11 +0300

    add via upload

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stability 24</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+
+    <script src="js/accordion.js"></script>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <link href="css/slick.css" rel="stylesheet" type="text/css">
+    <link href="css/slick-theme.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/media-queries.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="container">
+    <header class="header">
+        <img class="icon" src="media/header_logo.svg">
+        <nav>
+            <ul class="navbar">
+                <li><button type="button">Navitem1</button></li>
+                <li><button type="button">Navitem2</button></li>
+                <li><button type="button">Navitem3</button></li>
+            </ul>
+        </nav>
+        <button type="button" class="greenbutton">Button</button>
+    </header>
+
+    <main class="body">
+        <div class="block-1">
+            <img class="left-bubbles" src="media/left_pattern.png">
+            <img class="right-bubbles" src="media/right_pattern.png">
+            <div class="berries">
+                <div class="capital">
+                    <h1>Lorem ipsum, dolor sit amet consectetur</h1>
+                </div>
+
+                <div class="single-item">
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_headphones.svg"></svg>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_box.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_tablet.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_stability.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_headphones.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_box.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="single-item-arrows">
+                    <img class="arrow-left" src="media/arrow-left.svg">
+                    <img class="arrow-right" src="media/arrow-right.svg">
+                </div>
+
+                <div class="side-row">
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_headphones.svg"></svg>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_box.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="middle-row">
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_tablet.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_stability.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                <div class="side-row side-row_bottom">
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_headphones.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="item-box">
+                        <img class="item-box-pic" src="media/pic_box.svg">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                </ul>
+            </div>
+        </div>
+
+        <div class="block-2">
+            <div class="right-block">
+
+                <div class="capital">
+                    <h2>Lorem ipsum, dolor sit adipisicing elit.</h2>
+                    <p>Porro ab rerum omnis magnam eligendi error nobis dolore?</p>
+                </div>
+
+                <div class="accordion" id="accordion-1">
+                    <div class="accordion__block">
+                        <img src="media/icon_stability.svg">
+                        <div class="accordion__item">
+                            <div class="accordion__header">
+                                Lorem ipsum, dolor sit amet adipisicing elit.
+                            </div>
+                            <div class="accordion__body">
+                                Laboriosam quas, aut consectetur animi autem aliquid
+                                consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion__block">
+                        <img src="media/icon_led.svg">
+                        <div class="accordion__item">
+                            <div class="accordion__header">
+                                Lorem ipsum, dolor sit amet adipisicing elit.
+                            </div>
+                            <div class="accordion__body">
+                                Laboriosam quas, aut consectetur animi autem aliquid
+                                consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion__block">
+                        <img src="media/icon_light.svg">
+                        <div class="accordion__item">
+                            <div class="accordion__header">
+                                Lorem ipsum, dolor sit amet adipisicing elit.
+                            </div>
+                            <div class="accordion__body">
+                                Laboriosam quas, aut consectetur animi autem aliquid
+                                consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion__block">
+                        <img src="media/icon_splitter.svg">
+                        <div class="accordion__item">
+                            <div class="accordion__header">
+                                Lorem ipsum, dolor sit amet adipisicing elit.
+                            </div>
+                            <div class="accordion__body">
+                                Laboriosam quas, aut consectetur animi autem aliquid
+                                consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion__block">
+                        <img src="media/icon_power.svg">
+                        <div class="accordion__item">
+                            <div class="accordion__header">
+                                Lorem ipsum, dolor sit amet adipisicing elit.
+                            </div>
+                            <div class="accordion__body">
+                                Laboriosam quas, aut consectetur animi autem aliquid
+                                consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae delenit
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="left-block">
+                <div class="capital">
+                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h2>
+                </div>
+                <div class="left-block-content">
+                    <div class="quote-block">
+                        <img class="quote-block__image" src="media/profile_pic.svg">
+                        <div class="quote-block__text">
+                            <div class="quote-block__text_text">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur
+                                animi autem aliquid consequuntur suscipit exercitationem laborum cupiditate magnam eaque quae deleniti, iste nisi
+                                expedita, provident excepturi officia!
+                                Porro ab rerum omnis magnam eligendi error nobis dolore? Porro ab rerum omnis magnam eligendi error nobis dolore? Porro ab rerum omnis magnam eligendi error nobis dolore?
+                            </div>
+                            <div class="quote-block__text_name">
+                                Jane Doe
+                            </div>
+                        </div>
+                    </div>
+                    <div class="small-quote-blocks">
+                        <div class="quote-block-small__reverse">
+                            <img class="quote-block-small__image" src="media/profile_pic.svg">
+                            <div class="quote-block-small__text">
+                                <div class="quote-block__text_text">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur
+                                    animi autem aliquid
+                                </div>
+                                <div class="quote-block__text_name">
+                                    Jane Doe
+                                </div>
+                            </div>
+                        </div>
+                        <div class="quote-block-small">
+                            <img class="quote-block-small__image" src="media/profile_pic.svg">
+                            <div class="quote-block-small__text">
+                                <div class="quote-block__text_text">
+                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam quas, aut consectetur
+                                    animi autem aliquid
+                                </div>
+                                <div class="quote-block__text_name">
+                                    Jane Doe
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/slick.min.js"></script>
+    <script>
+        new ItcAccordion('#accordion-1', {
+            alwaysOpen: false
+        });
+    </script>
+    <script>
+        $('.single-item').slick({
+            dots: true,
+            arrows: true,
+            appendArrows: $('.single-item-arrows'),
+            nextArrow: $('.arrow-right'),
+            prevArrow: $('.arrow-left'),
+        })
+    </script>
+
+</body>
+
+</html>
